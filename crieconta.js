6445f5192095421eb14c2d2453427a8aa81b1081
Thomazi <103469661+Thomazi@users.noreply.github.com>
2022-06-08 23:46:39 -0300

    Create crieconta.js

---
+++ crieconta.js
@@ -0,0 +1,52 @@
+var email = document.getElementById('email')
+
+email.addEventListener('focus',()=>{
+     email.style.borderColor = "#516770"
+})
+
+email.addEventListener('blur',()=>{
+     email.style.borderColor = "#ccc"
+})
+
+var password = document.getElementById('password')
+
+password.addEventListener('focus',()=>{
+     password.style.borderColor = "#516770"
+})        
+
+password.addEventListener('blur',()=>{
+     password.style.borderColor = "#ccc"
+})
+
+var passwordd = document.getElementById('passwordd')
+
+passwordd.addEventListener('focus',()=>{
+     passwordd.style.borderColor = "#516770"
+})        
+
+passwordd.addEventListener('blur',()=>{
+     passwordd.style.borderColor = "#ccc"
+})
+
+var user = document.getElementById('user')
+
+user.addEventListener('focus',()=>{
+    user.style.borderColor = "#516770"
+})        
+
+user.addEventListener('blur',()=>{
+     user.style.borderColor = "#ccc"
+})
+
+
+var nomee = document.getElementById('nomee')
+
+nomee.addEventListener('focus',()=>{
+    nomee.style.borderColor = "#516770"
+})        
+
+nomee.addEventListener('blur',()=>{
+     nomee.style.borderColor = "#ccc"
+})
+
+
