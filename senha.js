2ec98db8ed3a3c5b9b9f0d2ef7c8f6c901cfa5bd
Thomazi <103469661+Thomazi@users.noreply.github.com>
2022-06-08 23:48:32 -0300

    Create senha.js

---
+++ senha.js
@@ -0,0 +1,29 @@
+var email = document.getElementById('email')
+
+email.addEventListener('focus',()=>{
+     email.style.borderColor = "#516770"
+})
+
+email.addEventListener('blur',()=>{
+     email.style.borderColor = "#ccc"
+})
+
+var password = document.getElementById('password')
+
+password.addEventListener('focus',()=>{
+     password.style.borderColor = "#516770"
+})        
+
+password.addEventListener('blur',()=>{
+     password.style.borderColor = "#ccc"
+})
+
+var passwordd = document.getElementById('passwordd')
+
+passwordd.addEventListener('focus',()=>{
+     passwordd.style.borderColor = "#516770"
+})        
+
+passwordd.addEventListener('blur',()=>{
+     passwordd.style.borderColor = "#ccc"
+})
